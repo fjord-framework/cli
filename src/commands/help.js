e0fee3db64feae6e54923cf5bdc12f6815370c0e
Sophie Chan <sophiecodes.jcwy@gmail.com>
2021-08-26 16:32:47 -0400

    updated help.js

--- src/commands/help.js
+++ src/commands/help.js
@@ -2,8 +2,9 @@ const { log } = require('console');
 
 module.exports = () => {
   log('Commands:');
-  log('\tfjord setup [optional: appName]\t\tCreates FjordSettings.json for customization.');
-  log('\tfjord deploy [optional: resourceName]\tDeploys the specified resource. If a resource is not specified, the entire stack will be deployed.');
-  log('\tfjord destroy [optional: resourceName]\tDestroys the specified resource. If a resource is not specified, the entire stack will be destroyed.');
-  log('\tfjord endpoints\t\t\t\tLists NAT gateways and API endpoints for deployed resources.');
+  log('  $ fjord setup [optional: appName]\t\tCreates FjordSettings.json for customization.\n');
+  log('  $ fjord deploy \t\t\t\tDeploys the entire stack to AWS.\n');
+  log('  $ fjord destroy [optional: resourceName]\tDestroys the specified resource.');
+  log('  \t\t\t\t\t\tIf a resource is not specified, the entire stack will be destroyed.\n')
+  log('  $ fjord endpoints\t\t\t\tLists NAT gateways and API endpoints for deployed resources.');
 };
